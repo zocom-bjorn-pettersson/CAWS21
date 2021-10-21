9401faf635b6a1d584202470ee37cb893e05eccf
Björn Pettersson <89526600+zocom-bjorn-pettersson@users.noreply.github.com>
2021-10-21 09:48:48 +0200

    OOIntro 2 med burkar, 2021-10-21

---
+++ OOIntro/Burk.java
@@ -0,0 +1,14 @@
+public class Burk {
+    int size;
+    String material;
+    String color;
+    String content;
+
+    // Contructor
+    public Burk(int newSize, String material, String color) {
+        size = newSize;
+        this.material = material;
+        this.color    = color;
+        this.content  = "unknown";
+    }
+}
--- OOIntro/MyOOIntro.java
+++ OOIntro/MyOOIntro.java
@@ -1,13 +1,28 @@
 public class MyOOIntro {
     public static void main(String[] args) {
         Animal djur = new Animal("Babba");
-        //djur.name = "Babba";
+/*
+        Burk fanta = new Burk();
+        Burk mineralvatten = new Burk();
 
-        Animal djur2 = new Animal("Bubba");
-        //djur2.name = "Bubbu";
+        fanta.size = 33;
+        fanta.color = "Yellow";
+        fanta.material = "Aluminium";
+        fanta.content = "Fanta";
 
-        djur2.test();
-        djur2.test();
-        djur.test();
+        mineralvatten.size = 50;
+        mineralvatten.color = "White";
+        mineralvatten.material = "Aluminium";
+        mineralvatten.content = "Bubbelvatten";
+ */
+
+        Burk fanta = new Burk(33, "Aluminium", "Yellow");
+        Burk mineralvatten = new Burk(50, "Aluminium", "White");
+
+        fanta.content = "Fanta";
+        mineralvatten.content = "Mineralvatten";
+
+        System.out.println("Fanta: " + fanta.size);
+        System.out.println("Mineralvatten: " + mineralvatten.size);
     }
 }
